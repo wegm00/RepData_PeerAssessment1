81aad702a84ee75129f87c60ed100007769c64ff
wegm00 <wegm@hotmail.com>
2020-12-16 22:33:57 -0600

    first load

--- PA1_template.md
+++ PA1_template.md
@@ -13,7 +13,8 @@ our zip file was decompress in our current working directory, if not we unzip
 the file and we assign it to our variable call **activity** and finally we 
 convert the column date in a date variable
 
-```{r, echo = TRUE}
+
+```r
 library(ggplot2)
 
 rfilename <- "activity.zip"
@@ -27,42 +28,65 @@ if (!file.exists("activity.cvs")) {
 activity <- read.csv("activity.csv")
 
 activity$date <- as.Date(activity$date, format = "%Y-%m-%d")
-
 ```
 
 ## What is mean total number of steps taken per day?
 for this part, first we Calculate the total number of steps taken per day, 
 ignoring the missing values
-```{r, echo = TRUE}
+
+```r
 stepsdaily <- aggregate(steps~date,activity,sum, na.rm=TRUE)
 ```
 
 next we plot a histogram of the total number of steps taken each day
-```{r, echo = TRUE}
+
+```r
 hist(stepsdaily$steps, main = "Total number of steps taken per day",
      xlab = "Total steps taken per day", col = "blue")
 ```
 
+![](PA1_template_files/figure-html/unnamed-chunk-3-1.png)<!-- -->
+
 finally we calculate Mean and median of total number of steps taken per day
-```{r, echo = TRUE}
+
+```r
 mean(stepsdaily$steps)
+```
+
+```
+## [1] 10766.19
+```
+
+```r
 median(stepsdaily$steps)
+```
+
+```
+## [1] 10765
 ```
 
 
 ## What is the average daily activity pattern?
 First, Time series plot of the 5 minute interval (x) and averaged number of 
 steps taken averaged across all days (y)
-```{r, echo = TRUE}
+
+```r
 stepsinterval <- aggregate(steps~interval,activity,mean, na.rm=TRUE)
 
 plot(steps~interval, data=stepsinterval, type="l",col = "red", 
      main = "Average steps per time interval" )
 ```
 
+![](PA1_template_files/figure-html/unnamed-chunk-5-1.png)<!-- -->
+
 Second, 5-minute interval (on average across all the days) with the maximum number of steps
-```{r, echo = TRUE}
+
+```r
 stepsinterval[which.max(stepsinterval$steps),]$interval
+```
+
+```
+## [1] 835
 ```
 
 
---
+++ codigo.R
@@ -0,0 +1,68 @@
+#Load Principle libraries
+library(ggplot2)
+library(dplyr)
+
+rfilename <- "activity.zip"
+
+
+# Checking if folder exists
+if (!file.exists("activity.cvs")) { 
+  unzip(rfilename) 
+}
+
+activity <- read.csv("activity.csv")
+stepsdaily <- aggregate(steps~date,activity,sum, na.rm=TRUE)
+hist(stepsdaily$steps, main = "Total number of steps taken per day",
+     xlab = "Total steps taken per day", col = "blue")
+
+mean(stepsdaily$steps)
+median(stepsdaily$steps)
+
+#next question
+
+stepsinterval <- aggregate(steps~interval,activity,mean, na.rm=TRUE)
+
+plot(steps~interval, data=stepsinterval, type="l",col = "red", 
+     main = "Average steps per time interval" )
+
+stepsinterval[which.max(stepsinterval$steps),]$interval
+
+# finding and replace missing data
+
+sum(is.na(activity$steps))
+
+
+
+activity$fullsteps <- ifelse(is.na(activity$steps), 
+                             round(stepsinterval$steps[match(activity$interval, stepsinterval$interval)],0),
+                             activity$steps)
+
+activityfull <- data.frame(steps=activity$fullsteps, interval=activity$interval, date=activity$date)
+
+stepsdailyfull <- aggregate(steps~date,activity,sum, na.rm=TRUE)
+hist(stepsdailyfull$steps, main = "Total number of steps taken per day",
+     xlab = "Total steps taken per day", col = "blue")
+
+mean(stepsdailyfull$steps)
+median(stepsdailyfull$steps)
+
+activityfull$date <- as.Date(activityfull$date, format = "%Y-%m-%d")
+
+activityfull$datetype <- sapply(activityfull$date, function(x) {
+  if (weekdays(x) == "sábado" | weekdays(x) =="domingo") 
+  {y <- "Weekend"} else 
+  {y <- "Weekday"}
+  y
+})
+
+stepsintervalfull <- aggregate(steps~interval+datetype,data=activityfull,FUN=mean)
+
+library(lattice)
+xyplot(steps ~ interval | datetype, activityfull, type = "l", layout = c(1, 2), 
+       xlab = "Interval", ylab = "Number of steps")
+
+g <-  ggplot(stepsintervalfull, aes(x = interval , y = steps, color = datetype)) +
+  geom_line() +
+  labs(title = "Average daily steps by type of date", x = "Interval", y = "Average number of steps") +
+  facet_wrap(~datetype, ncol = 1, nrow=2)
+print(g)
